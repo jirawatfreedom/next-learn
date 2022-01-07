8d6d7ce3d5b9a8e98425af86319301eb71b912d8
Jirawat  Maubkhuntod <Jirawatfreedom@gmail.com>
2022-01-07 13:12:07 +0700

    404

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -2,9 +2,24 @@ import {useRouter} from 'next/router'
 function ClientProjectPage() {
 	const router = useRouter();
 	console.log(router.query)
+	function loadProjectHandler() {
+		//load data from
+		router.push({
+			pathname: '/clients/[id]/[clientprojectid]',
+			query: {
+				id: 'max',
+				clientprojectid: 'projecta'
+			}
+		})
+	}
+
 	return (
 		<div>
+			<h1>
+
 			The Project of a Given Client
+			</h1>
+			<button onClick={loadProjectHandler}>Load Project A</button>
 		</div>
 	);
 }
--- pages/clients/index.js
+++ pages/clients/index.js
@@ -15,12 +15,15 @@ function ClientPage() {
 			<h1>The Client Page</h1>
 			<ul>
 
-				{clients.map(client =>(
+				{clients.map(client => (
 					<li key={client.id}>
-						<Link href={`/clients/${client.id}`}>{client.name}</Link>
+						<Link href={{
+							pathname: '/clients/[id]',
+							query: {id :client.id}
+						}}>{client.name}</Link>
 					</li>
 				))}
-				
+
 			</ul>
 		</div>
 	);
